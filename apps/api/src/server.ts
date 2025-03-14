a5e8f4e5a64787c2bf85fb6afb0514fd8eea3827
great-elephant <hoangtv2101@gmail.com>
2025-03-14 23:08:09 +0700

    1

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -12,7 +12,7 @@ export const createServer = (): Express => {
     .use(json())
     .use(cors())
     .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
+      return res.json({ message: `hello 1 ${req.params.name}` });
     })
     .get("/status", (_, res) => {
       return res.json({ ok: true });
